02a4aab7f855f958c0b8d6a2d308f3d5982f3268
Azizbek <azizbek.yoqubov.99@gmail.com>
2023-11-23 15:44:56 +0300

    api2 commit_2

--- bin/mock_api_project2.dart
+++ bin/mock_api_project2.dart
@@ -1,5 +1,5 @@
 import 'package:mock_api_project2/mock_api_project2.dart' as mock_api_project2;
 
 void main(List<String> arguments) {
-  print('Hello world: ${mock_api_project2.calculate()}!');
+  print('Hello world: ');
 }
